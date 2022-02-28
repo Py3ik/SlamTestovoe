15952c526486541f7e98e84878a3813a2ea2eda0
Py3ik <64283057+Py3ik@users.noreply.github.com>
2022-02-28 04:37:30 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.css"></link>
+    <link rel="stylesheet" href="/css/style.css"></link>
+    <link rel="stylesheet" href="/js/bootstrap.bundle.min.js"></link>
+    <title>Testovoe slam</title>
+</head>
+<body>
+    <div class="container col-6">
+        <div class="row">
+            <header class="sds">
+                <div class="col-md-auto">
+                    <img src="/img/logo.png" class=" navbar-brand mx-auto d-block logo" alt="">
+                </div>
+            </header>
+            <hr>
+                    <div class="sds col-2 col-md-2 sitebar">
+                        <ol>
+                            <li>
+                                <a href="/index.php">Главная</a> 
+                            </li>
+                            <li>
+                                <a href="/main.php">Задание 1</a> 
+                            </li>
+                        </ol>
+                    </div>
+                    <div class="col md-3 border-bottom box-shadow">
+                        <h1>Главная</h1>
+                    </div>              
+            <div class="col-md-auto fixed-bottom">
+                <footer class="fixed-bottom footer">
+                    <div class="container col-6 text-center sds cveta">
+                        <h5>Copiright by Nikita Kashko</h5>
+                    </div>
+                </footer>
+            </div>
+    </div>
+</body>
